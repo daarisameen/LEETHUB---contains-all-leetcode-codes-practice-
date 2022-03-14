768e43996f5034859532365a6385d9073a5148e4
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-03-14 10:06:20 +0530

    Time: 4 ms (57.40%), Space: 5.9 MB (74.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        x=pow(x,0.5);
-        return x;
+        
+        int ans=0;
+        int start=1,end=x;
+        while(start<=end)
+        {
+            int mid = (end-start)/2 + start;
+            
+            if(mid<=x/mid)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+                
+        }
+        return ans;
     }
 };
