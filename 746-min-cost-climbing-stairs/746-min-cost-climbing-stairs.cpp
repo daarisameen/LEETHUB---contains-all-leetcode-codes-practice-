a406ef628db86538bee936ef4be79805b0f5da14
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-03-18 23:52:47 +0530

    Time: 13 ms (30.85%), Space: 13.9 MB (27.60%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -11,7 +11,22 @@ public:
         return v[i]=min(l,r);
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int>v(cost.size(),-1);
-        return min(solve(cost,0,v),solve(cost,1,v));
+        vector<int>v(cost.size()+2,-1);
+        v[cost.size()]=0;
+        v[cost.size()+1]=0;
+        for(int i=cost.size()-1;i>=0;i--)
+        {
+            v[i]=min(cost[i]+v[i+1],cost[i]+v[i+2]);
+        }
+        
+        vector<int>z(cost.size()+2,-1);
+        z[cost.size()]=0;
+        z[cost.size()+1]=0;
+        for(int i=cost.size()-1;i>=1;i--)
+        {
+            z[i]=min(cost[i]+z[i+1],cost[i]+z[i+2]);
+        }
+        return min(v[0],z[1]);
+        // return min(solve(cost,0,v),solve(cost,1,v));
     }
 };
