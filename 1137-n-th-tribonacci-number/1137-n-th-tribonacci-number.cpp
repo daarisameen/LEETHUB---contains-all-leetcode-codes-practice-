e84f1b6737368820182bd1415730c2f4b5bb6c93
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-03-19 23:39:32 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.70%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -12,6 +12,13 @@ public:
     }
     int tribonacci(int n) {
         vector<int>v(n+1,-1);
-        return solve(n,v);
+        v[0]=0;
+        if(n>=1)
+        v[1]=1;
+        if(n>=2)
+        v[2]=1;
+        for(int i=3;i<n+1;i++)
+            v[i]=v[i-1]+v[i-2]+v[i-3];
+        return v[n];
     }
 };
