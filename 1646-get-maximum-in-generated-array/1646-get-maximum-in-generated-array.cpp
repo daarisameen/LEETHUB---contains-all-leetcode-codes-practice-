548c2a8cbc41cfe0b908e45450de2a43f4b584fe
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-03-20 09:14:36 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (25.89%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int n,int& mx)
+    int solve(int n,int& mx,vector<int>&v)
     {
         if(n==0)
             return 0;
@@ -9,22 +9,25 @@ public:
             mx=max(mx,1);
             return 1;
         }
+        if(v[n]!=-1)
+            return v[n];
         if(n%2==0)
         {
-            mx=max(mx,solve(n/2,mx));
-            return solve(n/2,mx);
+            mx=max(mx,solve(n/2,mx,v));
+            return v[n]=solve(n/2,mx,v);
         }
         else
         {
-            mx=max(mx,solve((n-1)/2,mx)+solve((n-1)/2+1,mx));
-            return solve((n-1)/2,mx)+solve((n-1)/2+1,mx);
+            mx=max(mx,solve((n-1)/2,mx,v)+solve((n-1)/2+1,mx,v));
+            return v[n]=solve((n-1)/2,mx,v)+solve((n-1)/2+1,mx,v);
         }
     }
     int getMaximumGenerated(int n) {
         int mx=0;
         int res=-1;
+        vector<int>v(n+1,-1);
         for(int i=0;i<n+1;i++)
-        int ans=solve(i,mx);
+        int ans=solve(i,mx,v);
         return mx;
     }
 };
