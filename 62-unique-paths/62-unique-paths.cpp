e18a9662d11ff5ae67abe697c0b162f17d579b59
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-04-01 22:06:15 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.45%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -11,26 +11,19 @@ public:
         return v[i][j]=solve(m,n,i+1,j,v)+solve(m,n,i,j+1,v);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>v(m+1,vector<int>(n+1,-1));
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==m-1 and j==n-1)
-                    v[i][j]=1;
-                if(i>m-1 || j>n-1)
-                    return v[i][j]=0;
-            }
-        }
+        vector<vector<int>>v(m+1,vector<int>(n+1,0));
+        v[m-1][n-1]=1;
         for(int i=m-1;i>=0;i--)
         {
             for(int j=n-1;j>=0;j--)
             {
-                if(i!=m-1 and j!=n-1)
+                if(i==m-1 and j==n-1)
+                    continue;
                 v[i][j]=v[i+1][j]+v[i][j+1];
             }
         }
-        return abs(v[0][0]);
+
+        return (v[0][0]);
         // return solve(m,n,0,0,v);
     }
 };
