f86cee3f9be58d72e13e71f1a2c48519d28a62ef
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-23 13:26:45 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.72%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int solve(int n,int i,vector<int>& v)
+    int solve(int i,int n,vector<int>&v)
     {
-        if(i==n)
-        {
-            return 1;
-        }
-        if(i>n)
-            return 0;
+        if(i>n)return v[i]=0;
+        if(i==n)return v[i]=1;
         if(v[i]!=-1)
             return v[i];
-        return v[i]=solve(n,i+1,v)+solve(n,i+2,v);
+        return v[i] = solve(i+1,n,v)+solve(i+2,n,v);
     }
     int climbStairs(int n) {
         vector<int>v(n+2,-1);
-        return solve(n,0,v);
+        return solve(0,n,v);
     }
 };
