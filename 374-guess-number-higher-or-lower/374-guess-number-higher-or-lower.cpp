0631ab27d46318e91f32f459b7ef3e5a805a3ec1
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-26 01:09:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (68.48%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -10,17 +10,19 @@
 class Solution {
 public:
     int guessNumber(int n) {
+        #include<iostream>
+        using namespace std;
         int start=1,end=n;
         while(start<=end)
         {
-            int mid=(end-start)/2 + start;
+            int mid=(end-start)/2+start;
             if(guess(mid)==0)
                 return mid;
-            else if(guess(mid)==-1)
-                end=mid-1;
-            else
+            if(guess(mid)==1)
                 start=mid+1;
+            else
+                end=mid-1;
         }
-        return 0;
+        return -1;
     }
 };
