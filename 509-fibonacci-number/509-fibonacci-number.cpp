40d42792b0f05081773fe0a0bac7fae97118f002
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-26 19:49:58 +0530

    Time: 6 ms (40.24%), Space: 6.2 MB (10.17%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int solve(int i)
+    int solve(int i,vector<int>&v)
     {
         if(i==1)
             return 1;
         if(i==0)
             return 0;
-        return solve(i-1)+solve(i-2);
+        if(v[i]!=-1)
+            return v[i];
+        return v[i]=solve(i-1,v)+solve(i-2,v);
     }
     int fib(int n) {
-        vector<int>v(n+1,0);
+        vector<int>v(n+1,-1);
         v[0]=0;
         if(n==0)
             return 0;
         v[1]=1;
         if(n==1)
             return 1;
-        return solve(n);
+        return solve(n,v);
     }
 };
