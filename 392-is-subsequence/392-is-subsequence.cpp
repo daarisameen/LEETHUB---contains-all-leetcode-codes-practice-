63c85d594f12f2923b78487358b0b3d9711c8e71
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-26 18:41:23 +0530

    Time: 161 ms (5.04%), Space: 280.3 MB (5.02%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-        bool isSubsequence(string s, string t) {
-        if(s.length()==0)
-            return true;
-        vector<vector<int>>v(s.length()+1,vector<int>(t.length()+1,-1));
-        for(int i=s.length();i>=0;i--)
-        {
-            for(int j=t.length();j>=0;j--)
+    bool solve(string s,string t,int i,int j)
+    {
+        if(i==s.length() || j==t.length())
             {
-                if(i==s.length()||j==t.length())
-                    v[i][j]=0;
-            }
-        }
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            for(int j=t.length()-1;j>=0;j--)
-            {
-                if(s[i]==t[j])
+                if(i==s.length())
                 {
-                    cout<<"HI";
-                    v[i][j]=1+v[i+1][j+1];
+                    return true;
                 }
                 else
-                    v[i][j]=v[i][j+1];
+                {
+                    return false;
+                }
             }
-            cout<<endl;
+        if(s[i]==t[j])
+        {
+            return true and solve(s,t,i+1,j+1);
         }
-
-         if(v[0][0]==s.length())
-             return true;
         else
-            return false;
+            return solve(s,t,i,j+1);
+    }
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        return solve(s,t,i,j);
     }
 };
