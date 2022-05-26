52191f59298667462bb076282eab32807326e582
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-26 20:39:39 +0530

    Time: 4 ms (31.95%), Space: 5.8 MB (90.27%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -4,8 +4,7 @@ public:
     {
         if(n==1)
             return false;
-        if(n==2)
-            return true;
+        
         if(solve(n-1)==false)
         {
             return true;
@@ -14,15 +13,7 @@ public:
             return false;
     }
     bool divisorGame(int n) {
-       vector<int>v(n+1,-1);
-        v[1]=false;
         if(n==1)
-            return false;
-        v[2]=true;
-        if(n==2)
-            return true;
-        v[3]=false;
-        if(n==3)
             return false;
         return solve(n);
         
