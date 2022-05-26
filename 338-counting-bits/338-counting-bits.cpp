83264f618a75f70815b29004eb720aabf63495ab
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-26 18:22:13 +0530

    Time: 3 ms (95.71%), Space: 7.9 MB (74.19%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+    void solve(int i,vector<int>&v)
+    {
+        if(i>=v.size())
+            return;
+        if(i%2==0)
+            v[i]=v[i/2];
+        else
+            v[i]=v[i/2]+1;
+        solve(i+1,v);
+    }
     vector<int> countBits(int n) {
         vector<int>v(n+1,0);
         if(n==0)
             return v;
         v[1]=1;
-        for(int i=2;i<n+1;i++)
-        {
-            if(i%2==0)
-                v[i]=v[i/2];
-            else
-                v[i]=1+v[i/2];
-        }
+        solve(2,v);
         return v;
     }
 };
