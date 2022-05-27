7a767ec435a17dc38bd72afc0a40b7c65ed28905
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-27 10:49:28 +0530

    Time: 165 ms (6.14%), Space: 304.6 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int solve(int num)
+    int solve(int num,vector<int>&v)
     {
         if(num==0)
             return 0;
+        if(v[num]!=-1)
+            return v[num];
         if(num%2==0)
-            return 1+solve(num/2);
+            return v[num]=1+solve(num/2,v);
         else
-            return 1+solve(num-1);
+            return v[num]=1+solve(num-1,v);
     }
     int numberOfSteps(int num) {
-        vector<int>v(num+1,0);
-        return solve(num);
+        vector<int>v(num+1,-1);
+        return solve(num,v);
     }
 };
