7a5cc2f81c03fbb6e7928a08dc7a8fdb7646f1fb
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-27 20:54:22 +0530

    Time: 121 ms (64.79%), Space: 41.9 MB (59.29%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.cpp
+++ 888-fair-candy-swap/888-fair-candy-swap.cpp
@@ -1,39 +1,42 @@
-// sum of aliceSizes = A, sum of bobSizes = B
-// A-x+y = B-y+x
-// (A-B+2*y)/2 = x 
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-        sort(aliceSizes.begin(),aliceSizes.end());
-        sort(bobSizes.begin(),bobSizes.end());
         int A=0,B=0;
-        vector<int>ans;
-        for(int i=0;i<aliceSizes.size();i++)
-            A += aliceSizes[i];
-        
-        for(int i=0;i<bobSizes.size();i++)
-            B += bobSizes[i];
-        
         for(int i=0;i<aliceSizes.size();i++)
         {
+            A+=aliceSizes[i];
+        }
+        for(int i=0;i<bobSizes.size();i++)
+        {
+            B+=bobSizes[i];
+        }
+        sort(bobSizes.begin(),bobSizes.end());
+        for(int i=0;i<aliceSizes.size();i++)
+        {
+            int x=aliceSizes[i];
             vector<int>v;
-            int z=(B-A+2*aliceSizes[i])/2;
-            v.push_back(aliceSizes[i]);
+            v.push_back(x);
+            int y=(B-A)/2+x;
+            if(y<=0)
+                continue;
             int start=0,end=bobSizes.size()-1;
             while(start<=end)
             {
-                int mid=(end-start)/2 + start;
-                if(bobSizes[mid]==z)
+                int mid=(end-start)/2+start;
+                if(bobSizes[mid]==y)
                 {
-                    v.push_back(z);
+                    v.push_back(y);
                     return v;
                 }
-                if(bobSizes[mid]<z)
+                if(bobSizes[mid]<y)
+                {
                     start=mid+1;
+                }
                 else
                     end=mid-1;
             }
         }
-        return ans;
+        vector<int>v;
+        return v;
     }
 };
