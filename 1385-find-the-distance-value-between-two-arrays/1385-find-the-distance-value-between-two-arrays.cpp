6cbec88396ac2f57f632f502a610aa2659ee6c9b
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-28 13:43:06 +0530

    Time: 24 ms (25.07%), Space: 13 MB (95.52%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
         sort(arr2.begin(),arr2.end());
-        int count=0;
+        int cnt=0;
         for(int i=0;i<arr1.size();i++)
         {
-            int ans=0;
+            bool flag=false;
+            int l=arr1[i]-d;
+            int r=arr1[i]+d;
+            // cout<<l<<" "<<r<<endl;
             int start=0,end=arr2.size()-1;
             while(start<=end)
             {
-                int mid=(end-start)/2 + start;
-                if(arr2[mid]<=arr1[i])
+                int mid=(end-start)/2+start;
+                if(l<=arr2[mid] and arr2[mid]<=r)
                 {
-                    ans=mid;
-                    if((mid+1)<arr2.size() and abs(arr2[mid+1]-arr1[i])<abs(arr2[mid]-arr1[i]))
-                        ans=mid+1;
-                    start=mid+1;
+                    // cout<<arr2[mid]<<" hi";
+                    flag=true;
+                    break;
                 }
-                else 
+                else if(arr2[mid]>r)
                     end=mid-1;
+                else if(arr2[mid]<l)
+                    start=mid+1;
             }
-
-            if(abs(arr2[ans]-arr1[i])>d)
-                count++;
+            if(flag)
+                continue;
+            cnt++;
         }
-        return count;
+        return cnt;
     }
 };
