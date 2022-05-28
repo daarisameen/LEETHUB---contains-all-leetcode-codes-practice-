8ab6a9252c7273ec09ea53756b5d6469fdf9fb9b
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-05-28 10:43:56 +0530

    Time: 25 ms (59.12%), Space: 18.5 MB (8.44%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<int>v(nums.size()+1,0);
+        for(int i=0;i<nums.size();i++)
+            v[nums[i]]=1;
+        for(int i=0;i<v.size();i++)
+            if(v[i]==0)
+                return i;
+        return -1;
+    }
+};
