5be8ae1defef60c7055723f7be7943c6dfbba337
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-06-03 10:07:17 +0530

    Time: 21 ms (43.69%), Space: 13.5 MB (11.65%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
+    void solve(vector<vector<int>>& matrix,int i,int j)
+    {
+        if(i<0 || j<0 || i>=matrix.size() || j>=matrix[0].size())
+            return;
+        matrix[i][j]=0;
+        solve(matrix,i-1,j);
+        solve(matrix,i+1,j);
+        solve(matrix,i,j-1);
+        solve(matrix,i,j+1);
+    }
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int>row;set<int>col;
+        map<int,int>row;map<int,int>col;
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    row.insert(i); col.insert(j);
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        int a=matrix.size();
-        int b=matrix[0].size();
-        vector<vector<int>>v(a,vector<int>(b,1));
-        for(int i=0;i<a;i++)
+        for(auto i:row)
         {
-            for(int j=0;j<b;j++)
-            {
-                if(row.find(i)!=row.end())
-                    v[i][j]=0;
-                else if(col.find(j)!=col.end())
-                    v[i][j]=0;
-                else 
-                    v[i][j]=matrix[i][j];
-                
-            }
+            for(int j=0;j<matrix[0].size();j++)
+                matrix[i.first][j]=0;
         }
-        matrix=v;
+        
+        for(auto j:col)
+        {
+            for(int i=0;i<matrix.size();i++)
+                matrix[i][j.first]=0;
+        }
+        
     }
 };
