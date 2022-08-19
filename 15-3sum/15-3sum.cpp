829f57f1f1cfbe940e5c5a9b592438ca359c0af0
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-08-19 18:45:05 +0530

    Time: 2941 ms (5.00%), Space: 242.4 MB (6.01%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        int n=nums.size();
         if(nums[0]==nums[nums.size()-1] and nums[0]==0)
         {
-            vector<vector<int>>ans(1,vector<int>(3,0));
-            return ans;
+            vector<vector<int>>v(1,vector<int>(3,0));
+            return v;
         }
-        int target=0;
-        int n = nums.size();
         set<vector<int>>sv;
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                int x = target-nums[i]-nums[j];
+                int x=0-nums[i]-nums[j];
                 if(binary_search(nums.begin()+j+1,nums.end(),x))
                 {
                     vector<int>v;
+                    v.push_back(x);
                     v.push_back(nums[i]);
                     v.push_back(nums[j]);
-                    v.push_back(x);
                     sort(v.begin(),v.end());
                     sv.insert(v);
                 }
