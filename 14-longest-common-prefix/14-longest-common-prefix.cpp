521738caa078c638bcdb1a6f1736fe65d8b2e666
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2022-09-13 21:44:40 +0530

    Time: 7 ms (57.50%), Space: 9.8 MB (10.67%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,59 +1,109 @@
+struct Node{
+  Node* links[26];
+    bool flag=false;
+    int cntstartswith=0;
+    Node* get(char ch)
+    {
+        return links[ch-'a'];
+    }
+    void put(char ch, Node* node)
+    {
+        links[ch-'a']=node;
+    }
+    bool containkey(char ch)
+    {
+        return links[ch-'a']!=NULL;
+    }
+    void setend()
+    {
+        flag=true;
+    }
+    void increaseprefix()
+    {
+        cntstartswith++;
+    }
+    int cntprefix()
+    {
+        return cntstartswith;
+    }
+};
+class Trie{
+    public:
+    Node* root;
+    Trie()
+    {
+        root=new Node();
+    }
+    void insert(string word)
+    {
+        Node* node = root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(!node->containkey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node=node->get(word[i]);
+            node->increaseprefix();
+        }
+        // node->setend();
+    }
+    int longestcommonprefix(string word)
+    {
+        Node* node = root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(node->containkey(word[i]))
+            {
+               node=node->get(word[i]); 
+            }
+            else
+                return 0;
+        }
+        return node->cntprefix();
+    }
+    bool prefixcheck(string word)
+    {
+        Node* node = root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(node->containkey(word[i]))
+            {
+               node=node->get(word[i]); 
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+    
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        map<int,char>mp;
-        if(strs.size()==1)
-            return strs[0];
-        string z="";
-        int l=INT_MAX;
+        Trie T;
+        for(int i=0;i<strs.size();i++)
+        {
+            T.insert(strs[i]);
+        }
+        string ans="";
+        bool flag=false;
         for(int i=0;i<strs[0].length();i++)
         {
-            mp[i]=strs[0][i];
-        }
-        // for(auto i:mp)
-        // {
-        //     cout<<i.first<<" ";
-        // }
-        // cout<<strs.size();
-        int m=strs[0].length();
-        for(int i=1;i<strs.size();i++)
-        {
-            // m=min(m,strs[i].length());
-            if(m>strs[i].length())
-                m=strs[i].length();
-            // cout<<m;
-            for(int j=0;j<strs[i].length();j++)
+            ans+=strs[0][i];
+            if(T.longestcommonprefix(ans)!=strs.size())
             {
-                // cout<<j<<" ";
-                    if(i==strs.size()-1)
-                    {
-                        if(j<l and j<m)
-                        {
-                            if(mp[j]==strs[i][j])
-                            {
-                                z+=mp[j];
-                            }
-                            else
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        
-                        if(mp[j]!=strs[i][j] and l==INT_MAX)
-                        {
-                            l=j;cout<<l;
-                            
-                        }
-                    }
-                
+                flag=true;
+                break;
             }
         }
-        if(strs.size()==1)
-            return strs[0];
+        // if(strs.size()==1)
+        //     return strs[0];
+        // cout<<T.prefixcheck(strs[0])<<" ";
+        if(flag)
+        return ans.substr(0,ans.size()-1);
         else
-        return z;
+            return ans;
     }
 };
-
-
